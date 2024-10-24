1e22e6d356cb28aacad05ab5bec165782f466666
Mateo Espitia <mateoespit@gmail.com>
2024-10-24 15:50:06 -0500

    Configure Render deployment

--- sensor_data/views.py
+++ sensor_data/views.py
@@ -5,21 +5,13 @@ from rest_framework.views import APIView
 from .models import SensorData
 from .serializers import SensorDataSerializer
 
-
 # Vista para recibir datos del sensor
 class SensorDataCreateAPIView(APIView):
     def post(self, request, *args, **kwargs):
-        # Depurar los datos que están siendo recibidos
-        print("Datos recibidos:", request.data)
-
-        # Crear un nuevo registro de datos
+        print("Datos recibidos:", request.data)  # Depurar los datos recibidos
         serializer = SensorDataSerializer(data=request.data)
-        
-        # Depurar los errores de validación, si existen
         if serializer.is_valid():
             serializer.save()
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_201_CREATED)
-        
-        # Si hay errores, depurar los errores del serializador
-        print("Errores de validación:", serializer.errors)
+        print("Errores de validación:", serializer.errors)  # Depurar los errores de validación
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
