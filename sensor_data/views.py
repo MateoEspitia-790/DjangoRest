cb2f106836c3bde27fc11aaa0a67e640d98cce5d
Mateo Espitia <mateoespit@gmail.com>
2024-10-24 15:37:39 -0500

    Configure Render deployment

--- sensor_data/views.py
+++ sensor_data/views.py
@@ -4,11 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import SensorData
 from .serializers import SensorDataSerializer
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
+
 
 # Vista para recibir datos del sensor
-@method_decorator(csrf_exempt, name='dispatch')
 class SensorDataCreateAPIView(APIView):
     def post(self, request, *args, **kwargs):
         # Crear un nuevo registro de datos
